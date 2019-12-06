574760b7cdce9d82520350fbfbc9499e218c46c0
187pm06722 <thy.187pm06722@vanlanguni.vn>
2019-12-06 22:31:17 +0700

    3rd

--- index/index/indexForm.Designer.cs
+++ index/index/indexForm.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // indexForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(772, 499);
+            this.Name = "indexForm";
+            this.Text = "moviegestForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion

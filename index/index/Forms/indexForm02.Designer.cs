069843ebab30705d5eb5d7720eee0145a30cce48
187pm06722 <thy.187pm06722@vanlanguni.vn>
2019-12-10 14:11:39 +0700

    add webBrowser

--- index/index/Forms/indexForm02.Designer.cs
+++ index/index/Forms/indexForm02.Designer.cs
@@ -36,8 +36,11 @@
             this.logoBox01 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logoBox01)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -124,11 +127,33 @@
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.webBrowser1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(199, 0);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(1171, 749);
             this.panel3.TabIndex = 2;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(7, 38);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1161, 708);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri(resources.GetString("webBrowser1.Url"), System.UriKind.Absolute);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(6, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Top rated movies from website";
             // 
             // indexForm02
             // 
@@ -142,6 +167,8 @@
             this.Text = "indexForm02";
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.logoBox01)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -155,5 +182,7 @@
         private System.Windows.Forms.LinkLabel btnYourFavorites;
         private System.Windows.Forms.LinkLabel btnBrowseForMovies;
         private System.Windows.Forms.LinkLabel btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }

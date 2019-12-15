e521e15c00dd7af294607ef786df0bfabd5a9099
phamquocduy1110@outlook.com <phamquocduy1110@outlook.com>
2019-12-15 20:23:22 +0700

    AddNewMovieForm decoration

--- index/index/Forms/AddNewMoviesForm.Designer.cs
+++ index/index/Forms/AddNewMoviesForm.Designer.cs
@@ -79,6 +79,10 @@ namespace index.Forms
             this.label1 = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
@@ -125,7 +129,7 @@ namespace index.Forms
             this.panel1.Controls.Add(this.lblBudget);
             this.panel1.Controls.Add(this.lblIndex);
             this.panel1.ForeColor = System.Drawing.Color.White;
-            this.panel1.Location = new System.Drawing.Point(12, 122);
+            this.panel1.Location = new System.Drawing.Point(49, 134);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(999, 397);
             this.panel1.TabIndex = 6;
@@ -477,7 +481,7 @@ namespace index.Forms
             this.panel3.AccessibleName = "";
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel3.Controls.Add(this.label1);
-            this.panel3.Location = new System.Drawing.Point(12, 28);
+            this.panel3.Location = new System.Drawing.Point(49, 40);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(999, 67);
             this.panel3.TabIndex = 8;
@@ -488,9 +492,9 @@ namespace index.Forms
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(-2, -4);
+            this.label1.Location = new System.Drawing.Point(-3, -2);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(999, 67);
+            this.label1.Size = new System.Drawing.Size(1042, 67);
             this.label1.TabIndex = 0;
             this.label1.Text = "Add New Movies";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -523,12 +527,48 @@ namespace index.Forms
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = false;
             // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(1, -4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 1142);
+            this.label2.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(4, -4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1148, 27);
+            this.label3.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(1077, -4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 1142);
+            this.label4.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label5.Location = new System.Drawing.Point(1, 607);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(1148, 23);
+            this.label5.TabIndex = 14;
+            // 
             // AddNewMoviesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(42)))), ((int)(((byte)(52)))));
-            this.ClientSize = new System.Drawing.Size(1026, 618);
+            this.ClientSize = new System.Drawing.Size(1100, 629);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.panel3);
@@ -587,5 +627,9 @@ namespace index.Forms
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnCancel;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
     }
 }
